f11796c9c2a097a445b5d4b8630ca83b93c46a20
Kamil Klawikowski <kamil.klawikowski03@gmail.com>
2025-10-01 20:56:37 +0200

    feat: add PageResult class for pagination

---
+++ apps/UsersService/src/UsersService.Domain/Common/PageResult.cs
@@ -0,0 +1,10 @@
+namespace UsersService.Domain.Common
+{
+    public class PageResult<T>
+    {
+        public IEnumerable<T> Items { get; set; } = Enumerable.Empty<T>();
+        public int TotalCount { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
